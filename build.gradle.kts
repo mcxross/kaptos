f25889e7f7966488908218ad694df338bf10cf9e
astinz <28899947+astinz@users.noreply.github.com>
2024-07-15 17:11:23 +0300

    refactor: use a better pluign for maven publication - we fully switch to catalog for dep mgmt - we switch from the inbuilt maven publication plugin to vanniktech

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ group = "xyz.mcxross.kaptos"
 version = "1.5.0-SNAPSHOT"
 
 plugins {
-  alias(libs.plugins.androidApplication) apply false
-  alias(libs.plugins.androidLibrary) apply false
-  alias(libs.plugins.jetbrainsCompose) apply false
-  alias(libs.plugins.kotlinMultiplatform) apply false
-  alias(libs.plugins.graphqlMultiplatform) apply false
-  alias(libs.plugins.kotlinSerialization) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.jetbrains.compose) apply false
+  alias(libs.plugins.kotlin.multiplatform) apply false
+  alias(libs.plugins.graphql.multiplatform) apply false
+  alias(libs.plugins.kotlin.serialization) apply false
   alias(libs.plugins.jvm) apply false
   alias(libs.plugins.dokka) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,16 +7,6 @@ pluginManagement {
     maven(url = "../repo")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
   }
-  plugins {
-    kotlin("jvm").version(extra["kotlin.version"] as String)
-    kotlin("multiplatform").version(extra["kotlin.version"] as String)
-    kotlin("plugin.serialization").version(extra["kotlin.version"] as String)
-    kotlin("android").version(extra["kotlin.version"] as String)
-    id("com.android.base").version(extra["agp.version"] as String)
-    id("com.android.application").version(extra["agp.version"] as String)
-    id("com.android.library").version(extra["agp.version"] as String)
-    id("org.jetbrains.dokka").version(extra["kotlin.version"] as String)
-  }
 }
 
 dependencyResolutionManagement {
