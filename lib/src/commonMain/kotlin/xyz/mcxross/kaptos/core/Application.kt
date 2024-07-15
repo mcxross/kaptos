f470a3035b77024371ceeb7fe4544c13d1704aaa
astinz <28899947+astinz@users.noreply.github.com>
2024-07-15 13:42:20 +0300

    feat: add gracefull recovery from errors - this feature enables toggling btn throwing errors and returning Option.None' - it defaults to false

---
+++ lib/src/commonMain/kotlin/xyz/mcxross/kaptos/core/Application.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024 McXross
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.mcxross.kaptos.core
+
+import xyz.mcxross.kaptos.model.Option
+
+/**
+ * The [Application] object is used to store global state for the SDK.
+ *
+ * @property graceFull [Boolean] indicating if the SDK is in graceFull mode. This is used to
+ *   determine if the SDK should throw exceptions or return [Option.None].
+ */
+internal object Application {
+  var graceFull: Boolean = false
+}
