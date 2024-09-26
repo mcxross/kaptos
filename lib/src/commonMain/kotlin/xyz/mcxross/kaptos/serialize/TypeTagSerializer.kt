3c63ecf1783d50a074283f219d53037acee2db03
astinz <28899947+astinz@users.noreply.github.com>
2024-09-26 10:06:58 +0300

    refactor: update `TypeTagSerializer` to internall encode `TypeTagVariants` - update `TypeTag` usages to reflect this change This commit updates the API to guard users from abusing the API. The `TypTagVariant` is now not a property of any given `TypeTag` type. This has all been moved into the serializer...

--- lib/src/commonMain/kotlin/xyz/mcxross/kaptos/serialize/TypeTagSerializer.kt
+++ lib/src/commonMain/kotlin/xyz/mcxross/kaptos/serialize/TypeTagSerializer.kt
@@ -31,185 +31,30 @@ object TypeTagSerializer : KSerializer<TypeTag> {
 
     when (value) {
       is TypeTagStruct -> {
-        encoder.beginStructure(descriptor).apply {
-          encodeSerializableElement(descriptor, 0, TypeTagStruct.serializer(), value)
-          endStructure(descriptor)
-        }
+        encoder.encodeEnum(TypeTagVariants.serializer().descriptor, TypeTagVariants.Struct.ordinal)
+        encoder.encodeSerializableValue(TypeTagStruct.serializer(), value)
       }
-      else -> encoder.encodeString(value.toString())
+      is TypeTagAddress -> encodeTypeTag(encoder, TypeTagVariants.Address, value)
+      is TypeTagBool -> encodeTypeTag(encoder, TypeTagVariants.Bool, value)
+      is TypeTagGeneric -> encodeTypeTag(encoder, TypeTagVariants.Generic, value)
+      is TypeTagReference -> encodeTypeTag(encoder, TypeTagVariants.Reference, value)
+      is TypeTagSigner -> encodeTypeTag(encoder, TypeTagVariants.Signer, value)
+      is TypeTagU8 -> encodeTypeTag(encoder, TypeTagVariants.U8, value)
+      is TypeTagU16 -> encodeTypeTag(encoder, TypeTagVariants.U16, value)
+      is TypeTagU32 -> encodeTypeTag(encoder, TypeTagVariants.U32, value)
+      is TypeTagU64 -> encodeTypeTag(encoder, TypeTagVariants.U64, value)
+      is TypeTagU128 -> encodeTypeTag(encoder, TypeTagVariants.U128, value)
+      is TypeTagU256 -> encodeTypeTag(encoder, TypeTagVariants.U256, value)
+      is TypeTagVector -> encodeTypeTag(encoder, TypeTagVariants.Vector, value)
     }
+  }
+
+  private fun encodeTypeTag(encoder: Encoder, variant: TypeTagVariants, value: TypeTag) {
+    encoder.encodeEnum(TypeTagVariants.serializer().descriptor, variant.ordinal)
+    encoder.encodeString(value.toString())
   }
 
   override fun deserialize(decoder: Decoder): TypeTag {
     return TypeTag.valueOf(decoder.decodeString())
-  }
-}
-
-object TypeTagAddressSerializer : KSerializer<TypeTagAddress> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagAddress", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagAddress) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Address.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagAddress {
-    return TypeTagAddress()
-  }
-}
-
-object TypeTagBoolSerializer : KSerializer<TypeTagBool> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagBool", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagBool) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Bool.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagBool {
-    return TypeTagBool()
-  }
-}
-
-object TypeTagGenericSerializer : KSerializer<TypeTagGeneric> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagGeneric", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagGeneric) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Generic.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagGeneric {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagReferenceSerializer : KSerializer<TypeTagReference> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagReference", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagReference) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Reference.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagReference {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagSignerSerializer : KSerializer<TypeTagSigner> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagSigner", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagSigner) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Signer.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagSigner {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagU8serializer : KSerializer<TypeTagU8> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagU8", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagU8) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.U8.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagU8 {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagU16Serializer : KSerializer<TypeTagU16> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagU16", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagU16) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.U16.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagU16 {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagU32Serializer : KSerializer<TypeTagU32> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagU32", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagU32) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.U32.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagU32 {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagU64Serializer : KSerializer<TypeTagU64> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagU64", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagU64) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.U64.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagU64 {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagU128Serializer : KSerializer<TypeTagU128> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagU128", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagU128) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.U128.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagU128 {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagU256Serializer : KSerializer<TypeTagU256> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagU256", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagU256) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.U256.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagU256 {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagVectorSerializer : KSerializer<TypeTagVector> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagVector", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagVector) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Vector.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagVector {
-    TODO("Not yet implemented")
-  }
-}
-
-object TypeTagStructSerializer : KSerializer<TypeTagStruct> {
-  override val descriptor: SerialDescriptor =
-    PrimitiveSerialDescriptor("TypeTagStruct", PrimitiveKind.STRING)
-
-  override fun serialize(encoder: Encoder, value: TypeTagStruct) {
-    encoder.encodeEnum(descriptor, TypeTagVariants.Struct.ordinal)
-  }
-
-  override fun deserialize(decoder: Decoder): TypeTagStruct {
-    TODO("Not yet implemented")
   }
 }
