3e3446341644fd8a1045db8c6d1934db2b3c655a
astinz <28899947+astinz@users.noreply.github.com>
2024-10-10 01:48:54 +0300

    tests tests tests...

--- lib/src/commonTest/kotlin/xyz/mcxross/kaptos/e2e/AccountTest.kt
+++ lib/src/commonTest/kotlin/xyz/mcxross/kaptos/e2e/AccountTest.kt
@@ -19,8 +19,12 @@ package xyz.mcxross.kaptos.e2e
 import kotlin.test.Test
 import kotlin.test.assertTrue
 import xyz.mcxross.kaptos.Aptos
+import xyz.mcxross.kaptos.account.Account
 import xyz.mcxross.kaptos.model.*
 import xyz.mcxross.kaptos.protocol.getAccountResource
+import xyz.mcxross.kaptos.util.APTOS_COIN
+import xyz.mcxross.kaptos.util.FUND_AMOUNT
+import xyz.mcxross.kaptos.util.getLocalNetwork
 import xyz.mcxross.kaptos.util.runBlocking
 
 class AccountTest {
@@ -103,5 +107,96 @@ class AccountTest {
         is Option.None -> assertTrue(false)
       }
     }
+  }
+
+  @Test
+  fun testGetAccountTransactions() = runBlocking {
+    val aptos = getLocalNetwork()
+    val alice = Account.generate()
+    aptos.fundAccount(alice.accountAddress, FUND_AMOUNT)
+    val bob = Account.generate()
+    val rawTxn =
+      aptos.buildTransaction.simple(
+        sender = alice.accountAddress,
+        data =
+          entryFunctionData {
+            function = "0x1::aptos_account::transfer"
+            functionArguments = functionArguments {
+              +bob.accountAddress
+              +U64(10UL)
+            }
+          },
+      )
+
+    val authenticator = aptos.sign(alice, rawTxn)
+    val response = aptos.submitTransaction.simple(rawTxn, authenticator)
+    val txn = aptos.waitForTransaction(HexInput(response.expect("").hash))
+    val accountTransactions = aptos.getAccountTransactions(accountAddress = alice.accountAddress)
+    assertTrue(
+      accountTransactions.expect("Couldn't get Account Txns")[0].expect("")[0] ==
+        txn.expect("Couldn't get Txn")
+    )
+  }
+
+  @Test
+  fun testGetAccountTransactionCount() = runBlocking {
+    val aptos = Aptos()
+    val alice = Account.generate()
+    aptos.fundAccount(alice.accountAddress, FUND_AMOUNT)
+    val accountTransactionsCount =
+      aptos.getAccountTransactionsCount(accountAddress = alice.accountAddress)
+    assertTrue(accountTransactionsCount.expect("").toInt() == 1)
+  }
+
+  @Test
+  fun testGetAccountCoinsData() = runBlocking {
+    val aptos = Aptos()
+    val alice = Account.generate()
+    aptos.fundAccount(alice.accountAddress, FUND_AMOUNT)
+    val accountCoinsData =
+      aptos
+        .getAccountCoinsData(accountAddress = alice.accountAddress)
+        .expect("Couldn't get account coins data")
+        .current_fungible_asset_balances[0]
+    assertTrue(accountCoinsData.amount == FUND_AMOUNT)
+    assertTrue(accountCoinsData.asset_type == "0x1::aptos_coin::AptosCoin")
+  }
+
+  @Test
+  fun testGetAccountCoinsCount() = runBlocking {
+    val aptos = Aptos()
+    val alice = Account.generate()
+    aptos.fundAccount(alice.accountAddress, FUND_AMOUNT)
+    val accountCoinsCount =
+      aptos
+        .getAccountCoinsCount(accountAddress = alice.accountAddress)
+        .expect("Couldn't get account coins data")
+    assertTrue(accountCoinsCount == 1)
+  }
+
+  @Test
+  fun testGetAccountCoinAmount() = runBlocking {
+    val aptos = Aptos()
+    val alice = Account.generate()
+    aptos.fundAccount(alice.accountAddress, FUND_AMOUNT)
+
+    val otherCoinAmount =
+      aptos
+        .getAccountCoinAmount(
+          accountAddress = alice.accountAddress,
+          MoveValue.MoveStructId("0x1::string::String"),
+        )
+        .expect("Couldn't get account coin amount")
+
+    assertTrue(otherCoinAmount == 0L)
+
+    val accountAPTAmount =
+      aptos
+        .getAccountCoinAmount(
+          accountAddress = alice.accountAddress,
+          coinType = MoveValue.MoveStructId(APTOS_COIN),
+        )
+        .expect("Couldn't get account coins data")
+    assertTrue(accountAPTAmount == FUND_AMOUNT)
   }
 }
